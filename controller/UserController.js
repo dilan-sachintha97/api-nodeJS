c2405f23dff585572e86469ad536d2684603e898
dilan-schintha97 <dil97an@gmail.com>
2023-09-04 22:17:34 +0530

    check user is already exits befor save new user

--- controller/UserController.js
+++ controller/UserController.js
@@ -3,27 +3,34 @@ const bcrypt = require('bcrypt')
 
 
 const register = (request,response)=>{
-    bcrypt.hash(request.body.password, 10, function(err, hash) {  // to encrypt password
 
-        const dto = new UserSchema({
-            name:request.body.name,
-            email:request.body.email,
-            password:hash
-        });
+    UserSchema.findOne({email:request.body.email}).then(result=>{
+        if (result == null){  // no same user in db
+            bcrypt.hash(request.body.password, 10, function(err, hash) {  // to encrypt password
+                const dto = new UserSchema({
+                    name:request.body.name,
+                    email:request.body.email,
+                    password:hash
+                });
 
-        dto.save().then(result =>{
-            let responseUserData = {
-                userEmail:result.email,
-                token:'****',
-                status:201,
-                message:'success'
-            }
-            response.status(201).json(responseUserData);
-        }).catch(error=>{
-            response.status(500).json(error);
-        })
+                dto.save().then(result =>{
+                    let responseUserData = {
+                        userEmail:result.email,
+                        token:'****',
+                        status:201,
+                        message:'success'
+                    }
+                    response.status(201).json(responseUserData);
+                }).catch(error=>{
+                    response.status(500).json(error);
+                })
 
-    });
+            });
+        }else{
+            // if user is already exit
+            response.status(409).json({'message':'Already exits'});
+        }
+    })
 
 
 }
